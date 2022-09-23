9021293d6e02d0ec39ed3992fcc7775f28625980
Kevin <kevin@aptoslabs.com>
2022-09-23 09:26:28 -0500

    [Tests] Add a test for init_module when republishing package

--- aptos-move/e2e-move-tests/tests/init_module.rs
+++ aptos-move/e2e-move-tests/tests/init_module.rs
@@ -1,7 +1,6 @@
 // Copyright (c) Aptos
 // SPDX-License-Identifier: Apache-2.0
 
-use aptos_types::account_address::AccountAddress;
 use e2e_move_tests::{assert_success, MoveHarness};
 use move_deps::move_core_types::parser::parse_struct_tag;
 use serde::{Deserialize, Serialize};
@@ -19,11 +18,11 @@ fn init_module() {
     let mut h = MoveHarness::new();
 
     // Load the code
-    let acc = h.new_account_at(AccountAddress::from_hex_literal("0xcafe").unwrap());
+    let acc = h.aptos_framework_account();
     assert_success!(h.publish_package(&acc, &common::test_dir_path("init_module.data/pack")));
 
     // Verify that init_module was called.
-    let module_data = parse_struct_tag("0xCAFE::test::ModuleData").unwrap();
+    let module_data = parse_struct_tag("0x1::test::ModuleData").unwrap();
     assert_eq!(
         h.read_resource::<ModuleData>(acc.address(), module_data.clone())
             .unwrap()
@@ -36,6 +35,30 @@ fn init_module() {
     assert_success!(h.publish_package(&acc, &common::test_dir_path("init_module.data/pack")));
     assert_eq!(
         h.read_resource::<ModuleData>(acc.address(), module_data)
+            .unwrap()
+            .global_counter,
+        42
+    );
+}
+
+#[test]
+fn init_module_when_republishing_package() {
+    let mut h = MoveHarness::new();
+
+    // Deploy a package that initially does not have the module that has the init_module function.
+    let acc = h.aptos_framework_account();
+    assert_success!(h.publish_package(
+        &acc,
+        &common::test_dir_path("init_module.data/pack_initial")
+    ));
+
+    // Now republish the package with the new module that has init_module.
+    assert_success!(h.publish_package(&acc, &common::test_dir_path("init_module.data/pack")));
+
+    // Verify that init_module was called.
+    let module_data = parse_struct_tag("0x1::test::ModuleData").unwrap();
+    assert_eq!(
+        h.read_resource::<ModuleData>(acc.address(), module_data.clone())
             .unwrap()
             .global_counter,
         42
